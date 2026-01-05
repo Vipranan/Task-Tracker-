9987cb02393dbc4a77bebd8c7877e0babc83fe71
Vipranan <vipranancr7@gmail.com>
2026-01-06 01:05:24 +0530

    Initial Commit

---
+++ TaskTracker/TaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerApp.swift
+//  TaskTracker
+//
+//  Created by Vipranan on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
